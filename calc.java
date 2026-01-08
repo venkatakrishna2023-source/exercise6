5249c8e94f9daf636e1d671df65e5adcf96f6e4a
venkatakrishna2023-source <venkata.krishna2023@vitstudent.ac.in>
2026-01-08 20:54:44 +0530

    file modified

--- calc.java
+++ calc.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 class calc{
-  public static void main [String()args]{
+  public static void main (String[]args){
     Scanner sc = new Scanner(System.in);
     int a = 10;
     int b = 15;
-    System.out.Println("Sum = "+(a+b));
+    System.out.println("difference = "+(a-b));
   }
 }
